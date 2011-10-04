c07d02c7f95701c7b6f7daa0f39f773778dc794e
ohkaiby <ohkaiby@gmail.com>
2011-10-03 21:44:46 -0500

    updated README files

--- iframe-coreg/include/db.php
+++ iframe-coreg/include/db.php
@@ -1,4 +1,4 @@
 <?php
-$dbh=mysql_connect ("localhost", "mysql_matt", "CC_1Mb4vx!!") or die ('I cannot connect to the database because: ' . mysql_error());
-mysql_select_db ("chreg", $dbh);
+$dbh=mysql_connect ("fake_host", "fake_user", "fake_pw") or die ('I cannot connect to the database because: ' . mysql_error());
+mysql_select_db ("fake_db", $dbh);
 ?>
